e9d26f4f2ff6878fb1ef5014c846d2ebfd471b80
UttejK <uttej09092001@gmail.com>
2023-04-30 21:16:24 +0530

    v002

--- src/App.jsx
+++ src/App.jsx
@@ -7,35 +7,34 @@ function App() {
   const [symbols, setSymbols] = useState([]);
   const [stockPrice, setStockPrice] = useState();
 
-  useEffect(() => {
-    let ws = new WebSocket("wss://stream.binance.com:9443/ws/ethusdt@trade");
-    ws.onmessage = (event) => {
-      let stockPrice = JSON.parse(event.data);
-      //   console.log((parseFloat(stockPrice?.p) * 80).toFixed(2)); // Development only
-      setStockPrice(stockPrice);
-    };
-  }, []);
+  // useEffect(() => {
+  //   let ws = new WebSocket("wss://stream.binance.com:9443/ws/ethusdt@trade");
+  //   ws.onmessage = (event) => {
+  //     let stockPrice = JSON.parse(event.data);
+  //     //   console.log((parseFloat(stockPrice?.p) * 80).toFixed(2)); // Development only
+  //     setStockPrice(stockPrice);
+  //   };
+  // }, []); // do not leave commented
 
-  useEffect(() => {
-    fetch("https://data.binance.com/api/v3/exchangeInfo")
-      .then((res) => res.json())
-      .then((data) => {
-        if (data?.symbols?.length) {
-          const usdtSymbols = [];
-          data.symbols.forEach(({ symbol }, i) => {
-            if (symbol.endsWith("USDT")) {
-              usdtSymbols.push(symbol);
-            }
-          });
+  // useEffect(() => {
+  //   fetch("https://data.binance.com/api/v3/exchangeInfo")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       if (data?.symbols?.length) {
+  //         const usdtSymbols = [];
+  //         data.symbols.forEach(({ symbol }, i) => {
+  //           if (symbol.endsWith("USDT")) {
+  //             usdtSymbols.push(symbol);
+  //           }
+  //         });
 
-          setSymbols(usdtSymbols);
-        }
-      });
-  }, []);
+  //         setSymbols(usdtSymbols);
+  //       }
+  //     });
+  // }, []);
 
   return (
     <>
-      <Overlay usdtSymbols={symbols} className="hidden" />
       <Navbar />
       <div className="max-w-md mx-auto h-screen w-screen flex items-center justify-stretch">
         <Container stockPrice={stockPrice} />
--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,10 +1,12 @@
 import Button from "./Button";
 import Binance from "./Binance";
+import Overlay from "./Overlay";
 import { useEffect, useState } from "react";
 
 function Container({ stockPrice }) {
   const [inValue, setInValue] = useState("");
   let [outValue, setOutValue] = useState("");
+  const [showOverlay, setShowOverlay] = useState(false);
 
   function handleChange(event) {
     setInValue(event.target.value);
@@ -15,18 +17,24 @@ function Container({ stockPrice }) {
   }
 
   return (
-    <div className=" bg-secondary p-8 rounded-2xl w-full">
-      <div className="flex justify-between items-center mb-8">
+    <div className="container bg-secondary p-8 rounded-2xl w-full flex items-center flex-col">
+      <div className="icon w-[70px] h-[70px] mb-8 -translate-y-16 rounded-full"></div>
+      <div className="flex justify-between items-center mb-2 w-full">
         <span>Current value</span>
         <Binance stockPrice={stockPrice} />
       </div>
-      <div className="flex justify-center items-center my-16"> DROPDOWN</div>
+      <button
+        className="flex justify-center items-center  mt-2 mb-8 bg-background2 w-full h-16 rounded-xl"
+        onClick={setShowOverlay(true)}
+      >
+        DROPDOWN
+      </button>
       <div className="block mb-8">
         <label htmlFor="" className="">
           Amount you want to invest
         </label>
         <input
-          className="appearance-none w-full bg-inherit h-16 rounded-xl border-[#6e56f8]/25 "
+          className="appearance-none focus:ring-0 focus:outline-none w-full bg-inherit h-16 rounded-xl border-[#6e56f8]/25 "
           type="number"
           placeholder="0.00"
           value={inValue}
