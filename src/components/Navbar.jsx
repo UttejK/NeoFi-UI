428b25d67c419d558bee639bbf8b496d8f4a54d4
UttejK <uttej09092001@gmail.com>
2023-05-01 11:41:05 +0530

    v005

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,14 @@ function Navbar() {
   return (
     <>
       <nav className="flex justify-between items-center bg-secondary px-12 py-4 fixed top-0 w-full">
-        <div>NeoFi</div>
+        <div className="flex items-center gap-x-4 font-bold text-2xl bg-clip-text text-transparent bg-gradient-to-r from-grad1 to-grad2">
+          <img
+            src="/NeoFi.svg"
+            className="h-[32px] w-[25px]"
+            alt="NeoFi_Logo"
+          />
+          <p>Neo Fi</p>
+        </div>
 
         <ul className="flex justify-center items-center gap-[50px] list-none ">
           <li>
